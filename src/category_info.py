42ba7038ffb54d924491a0a93e0b5cfb061385b0
VitaliyGubin <trofim-83@mail.ru>
2026-06-26 07:13:08 +0800

    добавлен category_info.py

---
+++ src/category_info.py
@@ -0,0 +1,24 @@
+from src.category import Category
+
+class CategoryInfo:
+    """Представляет товары из категории."""
+    category: Category
+    _products: list
+    _index: int
+
+    def __init__(self, category: Category):
+        self.category = category
+        self._index = 0
+        self._products = self.category.products.split('\n')
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        if self._index < len(self._products):
+            prod = self._products[self._index]
+            self._index += 1
+            return prod
+        else:
+            raise StopIteration
