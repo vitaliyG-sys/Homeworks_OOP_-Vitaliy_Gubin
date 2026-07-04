d93627f2382d77b43bf9504bd38b270733c82115
VitaliyGubin <trofim-83@mail.ru>
2026-07-04 08:43:46 +0800

    правки линтером в category_info.py

--- src/category_info.py
+++ src/category_info.py
@@ -1,21 +1,25 @@
+from typing import Any
+
 from src.category import Category
+
 
 class CategoryInfo:
     """Представляет товары из категории."""
+
     category: Category
     _products: list
     _index: int
 
-    def __init__(self, category: Category):
+    def __init__(self, category: Category) -> None:
         self.category = category
         self._index = 0
-        self._products = self.category.products.split('\n')
+        self._products = self.category.products.split("\n")
 
-    def __iter__(self):
+    def __iter__(self) -> CategoryInfo:
         self._index = 0
         return self
 
-    def __next__(self):
+    def __next__(self) -> Any:
         if self._index < len(self._products):
             prod = self._products[self._index]
             self._index += 1
